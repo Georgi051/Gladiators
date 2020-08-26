363f6860f78a6a94a89ee85df47c27028a75509a
Georgi051 <54109556+Georgi051@users.noreply.github.com>
2020-08-26 15:27:00 +0300

    .

---
+++ src/main/java/project/gladiators/config/ApplicationSecurityConfiguration.java
@@ -0,0 +1,46 @@
+package project.gladiators.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .csrfTokenRepository(csrfTokenRepository())
+             .and()
+                .authorizeRequests()
+                .antMatchers("/", "/users/login", "/users/register").anonymous()
+                .antMatchers("/css/**", "/js/**").permitAll()
+                .anyRequest().authenticated()
+             .and()
+                .formLogin()
+                .loginPage("/users/login").permitAll()
+                .usernameParameter("username")
+                .passwordParameter("password")
+                .defaultSuccessUrl("/home")
+             .and()
+                .logout()
+                .logoutSuccessUrl("/").permitAll()
+             .and()
+                .exceptionHandling()
+                .accessDeniedPage("/");
+    }
+
+    private CsrfTokenRepository csrfTokenRepository() {
+        HttpSessionCsrfTokenRepository repository =
+                new HttpSessionCsrfTokenRepository();
+        repository.setSessionAttributeName("_csrf");
+        return repository;
+    }
+}
