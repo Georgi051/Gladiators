582418eaeea134837cbeeeafd1f471cb7802b276
nikolayg92 <apokalipsys@mail.bg>
2020-08-31 08:46:38 +0300

    RoleChangeBindingModel.java added

---
+++ src/main/java/project/gladiators/model/bindingModels/RoleChangeBindingModel.java
@@ -0,0 +1,14 @@
+package project.gladiators.model.bindingModels;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class RoleChangeBindingModel {
+
+    private String id;
+    private String role;
+}
--- src/main/java/project/gladiators/model/bindingModels/UserRegisterBindingModel.java
+++ src/main/java/project/gladiators/model/bindingModels/UserRegisterBindingModel.java
@@ -1,16 +1,20 @@
 package project.gladiators.model.bindingModels;
 
+import com.sun.istack.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import javax.validation.constraints.Email;
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class UserRegisterBindingModel {
+
     private String username;
     private String password;
     private String confirmPassword;
+    @Email
     private String email;
 
 }
