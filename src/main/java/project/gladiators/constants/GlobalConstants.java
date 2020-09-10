22aedb88d22fde65112b663b8ed026bb94a02d3f
Georgi051 <54109556+Georgi051@users.noreply.github.com>
2020-09-10 13:29:52 +0300

    Little changes with exercise verification.

--- src/main/java/project/gladiators/constants/GlobalConstants.java
+++ src/main/java/project/gladiators/constants/GlobalConstants.java
@@ -2,9 +2,6 @@ package project.gladiators.constants;
 
 public class GlobalConstants {
     public static final String MUSCLES_FILE_PATH = "src/main/resources/json files/muscles.json";
-
-    public static final String WORKOUTS_FILE_PATH = "src/main/resources/json files/workouts.json";
-
     public static final String EXERCISES_FILE_PATH = "src/main/resources/json files/exercises.json";
 
 }
--- src/main/java/project/gladiators/model/bindingModels/ExerciseEditBindingModel.java
+++ src/main/java/project/gladiators/model/bindingModels/ExerciseEditBindingModel.java
@@ -8,16 +8,19 @@ import project.gladiators.model.entities.Muscle;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.Set;
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class ExerciseEditBindingModel {
+    @Size(min = 3, message = "Exercise name must be at least 3 characters")
     private String name;
+    @Size(min = 30, max = 1000, message = "The description must be between 30 and 1000 characters!")
     private String description;
-    @NotNull
+    @NotNull(message = "Please add exercise picture.")
     private MultipartFile imageUrl;
-    @NotEmpty(message = "You must select muscle one or more")
+    @NotEmpty(message = "You must select muscle one or more.")
     private Set<Muscle> muscles;
 }
